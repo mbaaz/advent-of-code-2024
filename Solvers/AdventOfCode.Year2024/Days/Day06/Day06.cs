a9af093630bbace222bc77833f0b3ccc390a0bb3
Matts Baaz <matts.baaz@kapero.se>
2024-12-08 23:12:16 +0100

    Done!

--- Solvers/AdventOfCode.Year2024/Days/Day06/Day06.cs
+++ Solvers/AdventOfCode.Year2024/Days/Day06/Day06.cs
@@ -2,28 +2,26 @@
 
 // This is my solution to the Advent of Code challenge!
 // <see>https://adventofcode.com/2024/day/6</see>
-[DaySolution(Day = 6, IsActive = false)]
+[DaySolution(Day = 6, IsActive = true)]
 public class Day06() : DaySolution(day: 6), IDaySolutionImplementation
 {
     public override void RunPart1(bool isTest, string[] input, Action<OutputMessage> output)
     {
-        var data = input.ParseToDay06Data();
+        var (map, guard) = input.ParseToDay06Data();
+        map.SimulateGuardMovements(guard, out var totalMovements, out var isLooped);
+        var visitedTiles = map.GetVisitedTilesCount();
 
-        output(new("Result", "[not yet defined]"));
+        output(new("Total movements made", $"{totalMovements:n0}"));
+        output(new("Is Guard path looped", isLooped ? "true" : "false"));
+        output(new("Total of visited tiles", $"{visitedTiles:n0}"));
     }
 
     public override void RunPart2(bool isTest, string[] input, Action<OutputMessage> output)
     {
-        var data = input.ParseToDay06Data();
+        var (map, guard) = input.ParseToDay06Data();
+        var obstructionPositions = map.CalculateHowManyPositionsForObstructionToPlaceGuardInLoop(guard, out var positionsChecked);
 
-        output(new("Result", "[not yet defined]"));
-    }
-}
-
-public static class Day06Extensions
-{
-    public static List<string> ParseToDay06Data(this string[] input)
-    {
-        return input.ToList();
+        output(new("Positions checked", $"{positionsChecked:n0}"));
+        output(new("Number or positions for obstructions to loop the guard", $"{obstructionPositions:n0}"));
     }
 }
