4a7c7ddf7232263ff5edd4a5e2e46be18cb862a7
Matts Baaz <matts.baaz@kapero.se>
2024-12-14 18:36:53 +0100

    Day 08 Solution This will successfully solve day 08 puzzles!

---
+++ Solvers/MBZ.AdventOfCode.Year2024/Days/Day08/City.cs
@@ -0,0 +1,112 @@
+﻿namespace MBZ.AdventOfCode.Year2024.Day08;
+
+public record City(CityTile[][] Tiles)
+{
+    public CityTile? GetTile(Position position) =>
+        IsValidPosition(position) ? Tiles[position.RowIndex][position.ColumnIndex] : null;
+
+    private bool IsValidPosition(Position position) =>
+        position.RowIndex >= 0 && 
+        position.RowIndex < Tiles.Length &&
+        position.ColumnIndex >= 0 && 
+        position.ColumnIndex < Tiles[position.RowIndex].Length
+    ;
+
+    public void CalculateAntiNodes(bool extended = false)
+    {
+        var frequencies = GetFrequencies();
+        foreach (var frequency in frequencies)
+        {
+            CalculateAntiNodes(frequency, extended);
+        }
+    }
+
+    private void CalculateAntiNodes(char frequency, bool extended)
+    {
+        // Fetch all antennas with the given frequency
+        var antennaPositions = GetAntennaPositions(frequency);
+
+        // No need to calculate anti-nodes if there is only one antenna
+        if (antennaPositions.Count <= 1)
+        {
+            return;
+        }
+
+        // Calculate anti-nodes
+        for (var i = 0; i < antennaPositions.Count - 1; i++)
+        {
+            for (var j = i + 1; j < antennaPositions.Count; j++)
+            {
+                var antennaPosition1 = antennaPositions[i];
+                var antennaPosition2 = antennaPositions[j];
+                var distance = antennaPosition1.DistanceTo(antennaPosition2);
+
+
+                if (!extended)
+                {
+                    MarkAntiNode(antennaPosition1.Subtract(distance));
+                    MarkAntiNode(antennaPosition2.Add(distance));
+                }
+                
+                if (extended)
+                {
+                    var antiNodePosition = antennaPosition1;
+                    while (MarkAntiNode(antiNodePosition))
+                    {
+                        antiNodePosition = antiNodePosition.Subtract(distance);
+                    }
+
+                    antiNodePosition = antennaPosition2;
+                    while (MarkAntiNode(antiNodePosition))
+                    {
+                        antiNodePosition = antiNodePosition.Add(distance);
+                    }
+                }
+            }
+        }
+    }
+
+    private bool MarkAntiNode(Position position)
+    {
+        var tile = GetTile(position);
+        if (tile == null)
+        {
+            return false;
+        }
+
+        // No need to update the tile if it is already an anti-node
+        if (!tile.IsAntiNode)
+        {
+            // Update the tile to be an anti-node
+            Tiles[position.RowIndex][position.ColumnIndex] = tile.AsAntiNode();
+        }
+
+        return true;
+    }
+
+    private List<Position> GetAntennaPositions(char frequency) =>
+        Tiles
+            .SelectMany(t => t)
+            .Where(t => t.HasAntenna && t.Frequency == frequency)
+            .Select(t => t.Position)
+            .ToList()
+    ;
+
+    private List<char> GetFrequencies() =>
+        Tiles
+            .SelectMany(t => t)
+            .Where(t => t.HasAntenna)
+            .Select(t => t.Frequency)
+            .Distinct()
+            .ToList()
+    ;
+    
+    public int CountAntiNodes() =>
+        Tiles
+            .SelectMany(t => t)
+            .Count(t => t.IsAntiNode)
+    ;
+
+    public override string ToString() =>
+        string.Join(Environment.NewLine, Tiles.Select(tileRow => string.Join("", tileRow.Select(t => t))));
+}
---
+++ Solvers/MBZ.AdventOfCode.Year2024/Days/Day08/Day08Extensions.cs
@@ -0,0 +1,29 @@
+﻿namespace MBZ.AdventOfCode.Year2024.Day08;
+
+public static class Day08Extensions
+{
+    public static City ParseToDay08Data(this string[] input)
+    {
+        var cityTiles = new List<CityTile[]>();
+        for (var rowIndex = 0; rowIndex < input.Length; rowIndex++)
+        {
+            if(string.IsNullOrWhiteSpace(input[rowIndex]))
+                continue;
+            cityTiles.Add(ParseLine(rowIndex, input[rowIndex].Trim()));
+        }
+        return new City(cityTiles.ToArray());
+    }
+
+    private static CityTile[] ParseLine(int rowIndex, string line)
+    {
+        var cityTiles = new List<CityTile>();
+        for (var columnIndex = 0; columnIndex < line.Length; columnIndex++)
+        {
+            var position = new Position(rowIndex, columnIndex);
+            var cityTile = new CityTile(position, line[columnIndex]);
+            cityTiles.Add(cityTile);
+        }
+        return cityTiles.ToArray();
+    }
+
+}
