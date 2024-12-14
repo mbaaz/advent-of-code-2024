3a62f9250ce14bb2fa6acd84f5b2791a85bf8921
Matts Baaz <matts.baaz@kapero.se>
2024-12-14 18:35:46 +0100

    Added projects for previous years

---
+++ Solvers/MBZ.AdventOfCode.Year2016/GlobalUsings.cs
@@ -0,0 +1,7 @@
+// Global using directives
+
+global using MBZ.AdventOfCode.Core;
+global using MBZ.AdventOfCode.Core.Extensions;
+global using MBZ.AdventOfCode.Core.Infrastructure;
+global using MBZ.AdventOfCode.Core.Output;
+global using Microsoft.Extensions.DependencyInjection;
---
+++ Solvers/MBZ.AdventOfCode.Year2017/AppStartup.cs
@@ -0,0 +1,25 @@
+﻿using MBZ.AdventOfCode.Core.Configuration;
+using Microsoft.Extensions.Configuration;
+
+namespace MBZ.AdventOfCode.Year2017;
+
+public class AppStartup : AppStartupBase, IAppStartup
+{
+    public override IConfigurationBuilder ConfigureSettings(IConfigurationBuilder configurationBuilder)
+    {
+        return configurationBuilder
+            .AddJsonFile("FestiveAppConfig.json")
+        ;
+    }
+
+    public override void ConfigureServices(IServiceCollection services)
+    {
+        
+    }
+
+    public override void Configure(IServiceProvider services)
+    {
+
+    }
+
+}
