35e2032f08b29fb6cd5f3f2ce80213f5f97e9225
Matts Baaz <matts.baaz@kapero.se>
2024-12-20 23:21:46 +0100

    Day 10 Solution (#23)
    This will solve day 10 puzzles successfully!

---
+++ Solvers/MBZ.AdventOfCode.Year2024/Days/Day10/Day10Extensions.cs
@@ -0,0 +1,26 @@
+﻿namespace MBZ.AdventOfCode.Year2024.Day10;
+
+internal static class Day10Extensions
+{
+    public static TrailMap ParseToDay10Data(this string[] input)
+    {
+        var mapData = new TrailMapTile[input.Length][];
+        for (var rowIndex = 0; rowIndex < input.Length; rowIndex++)
+        {
+            mapData[rowIndex] = ParseRow(input[rowIndex], rowIndex);
+        }
+        return new TrailMap(mapData);
+    }
+
+    private static TrailMapTile[] ParseRow(string row, int rowIndex)
+    {
+        var mapTiles = new TrailMapTile[row.Length];
+        for (var columnIndex = 0; columnIndex < row.Length; columnIndex++)
+        {
+            var position = new Position(rowIndex, columnIndex);
+            var value = short.Parse(row[columnIndex].ToString());
+            mapTiles[columnIndex] = new TrailMapTile(position, value);
+        }
+        return mapTiles;
+    }
+}
---
+++ Solvers/MBZ.AdventOfCode.Year2024/Days/Day10/TrailMap.cs
@@ -0,0 +1,60 @@
+﻿namespace MBZ.AdventOfCode.Year2024.Day10;
+
+public record TrailMap : Map<TrailMapTile>
+{
+    public TrailMap(TrailMapTile[][] mapData) : base(mapData)
+    {
+    }
+
+    public IEnumerable<TrailMapTile> GetTrailHeads() =>
+        GetTiles().Where(tile => tile.IsTrailHead).ToList();
+
+    public int CalculateScoreForTrailHead(TrailMapTile trailHead)
+    {
+        var currentHeight = trailHead.Height;
+        var currentTiles = new[]{ trailHead };
+        while (currentHeight < TrailMapTile.TRAIL_PEAK_HEIGHT)
+        {
+            var heightToFind = currentHeight + 1;
+            currentTiles = currentTiles
+                    .SelectMany(tile => GetNeighbours(tile.Position))
+                    .Where(tile => tile.Height == heightToFind)
+                    .DistinctBy(tile => tile.Position)
+                    .ToArray()
+                ;
+            currentHeight++;
+        }
+        return currentTiles.Length;
+    }
+
+    public int CalculateScoreForTrailHeads() =>
+        GetTrailHeads()
+            .Select(CalculateScoreForTrailHead)
+            .Sum();
+
+    public int CalculateRatingForTrailHead(TrailMapTile trailHead)
+    {
+        var currentHeight = trailHead.Height;
+        var paths = new List<List<TrailMapTile>> { new List<TrailMapTile> { trailHead } };
+        while (currentHeight < TrailMapTile.TRAIL_PEAK_HEIGHT)
+        {
+            var heightToFind = currentHeight + 1;
+            var newPaths = paths
+                    .SelectMany(path => 
+                        GetNeighbours(path.Last().Position)
+                            .Where(tile => tile.Height == heightToFind)
+                            .Select(tile => path.Append(tile).ToList())
+                    )
+                    .ToList()
+                ;
+            paths = newPaths;
+            currentHeight++;
+        }
+        return paths.Count;
+    }
+
+    public int CalculateRatingForTrailHeads() =>
+        GetTrailHeads()
+            .Select(CalculateRatingForTrailHead)
+            .Sum();
+}
